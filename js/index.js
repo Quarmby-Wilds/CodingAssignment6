0eb9d02b0f78c8b1e169d93bd330adfde34c6bca
Quarmby-Wilds <cedar.quarmby@bellevuecollege.edu>
2025-10-25 15:22:33 -0700

    Remove function

--- js/index.js
+++ js/index.js
@@ -1,37 +1,32 @@
-function bnj() {
-    // Welcome prompt
+// Welcome prompt
 
-    let name = prompt("Welcome to Bear, Ninja, Hunter. Please enter your name.")
-    alert("Hi, " + name + ". Let's get started.")
+let name = prompt("Welcome to Bear, Ninja, Hunter. Please enter your name.")
+alert("Hi, " + name + ". Let's get started.")
 
-    // Start game
+// Start game
 
-    let choice = prompt("Please enter Bear, Ninja, or Hunter.").toLocaleLowerCase()
+let choice = prompt("Please enter Bear, Ninja, or Hunter.").toLocaleLowerCase()
 
-    while (choice != "bear" && choice != "ninja" && choice != "hunter") {
-        choice = prompt("Uh oh! That wasn't one of the choices. Please choose Bear, Ninja, or Hunter.").toLocaleLowerCase()
-    }
-
-    // Generate computer answer
-
-    let compChoice
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        compChoice = "Bear";
-    } else if (randomNumber === 2) {
-        compChoice = "Ninja";
-    } else {
-        compChoice = "Hunter";
-    }
-
-    let results = name + ", you picked " + choice + ".\nThe computer picked " + compChoice
-
-    const resultElement = document.getElementById("results")
-    resultElement.innerHTML = results
-
-    console.log(name + ", you picked " + choice)
-    console.log("The computer picked " + compChoice)
-
+while (choice != "bear" && choice != "ninja" && choice != "hunter") {
+    choice = prompt("Uh oh! That wasn't one of the choices. Please choose Bear, Ninja, or Hunter.").toLocaleLowerCase()
 }
 
-document.addEventListener("DOMContentLoaded", bnj)
+// Generate computer answer
+
+let compChoice
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+if (randomNumber === 1) {
+    compChoice = "Bear";
+} else if (randomNumber === 2) {
+    compChoice = "Ninja";
+} else {
+    compChoice = "Hunter";
+}
+
+let results = name + ", you picked " + choice + ".\nThe computer picked " + compChoice
+
+const resultElement = document.getElementById("results")
+resultElement.innerHTML = results
+
+console.log(name + ", you picked " + choice)
+console.log("The computer picked " + compChoice)
